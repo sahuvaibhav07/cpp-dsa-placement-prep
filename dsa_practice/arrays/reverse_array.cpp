fe771f4203690a479afd4e47de8eb0b150255612
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-04-26 19:35:34 +0530

    day 4 array completed

---
+++ dsa_practice/arrays/reverse_array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    cout << "Enter 5 numbers:\n";
+
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reverse Array:\n";
+
+    for(int i = 4; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
