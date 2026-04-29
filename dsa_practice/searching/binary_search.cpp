8a2c461ccddacf66db92de941cf421f26e9bc227
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-04-29 21:19:47 +0530

    update

--- dsa_practice/searching/binary_search.cpp
+++ dsa_practice/searching/binary_search.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-// Binary Search - O(log n)
-int binarySearch(vector<int>& arr, int target) {
-    int left = 0, right = arr.size() - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) return mid;
-        else if (arr[mid] < target) left = mid + 1;
-        else right = mid - 1;
-    }
-    return -1;
-}
+int main(){
+    int arr[5] = {10, 20, 30, 40, 50,};
+    int key = 30;
 
-int main() {
-    vector<int> arr = {1, 3, 5, 7, 9, 11, 13, 15};
-    int target = 7;
-    
-    cout << "Binary Search Example" << endl;
-    cout << "Array: ";
-    for (int x : arr) cout << x << " ";
-    cout << endl;
-    
-    cout << "Target: " << target << endl;
-    int result = binarySearch(arr, target);
-    
-    if (result != -1) {
-        cout << "Found at index: " << result << endl;
-    } else {
-        cout << "Not found" << endl;
+    int start = 0;
+    int end = 4;
+    bool found = false;
+
+    while(start <= end){
+        int mid = (start + end) /2;
+
+        if(arr[mid] == key){
+            found = true;
+            break;
+        }
+        else if(arr[mid] < key){
+            start = mid +1;
+        }
+        else {
+            end = mid -1;
+        }
     }
-    
+    if (found) 
+    cout << "Element Found" ;
+
+    else 
+    cout << "Element Not Found";
+
     return 0;
 }
