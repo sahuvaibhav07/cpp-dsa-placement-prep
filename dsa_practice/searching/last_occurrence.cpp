ca35ce49dfa69db9511fa39cfbc5e7998b0819cf
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-04-30 22:30:21 +0530

    update

---
+++ dsa_practice/searching/last_occurrence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[6] = {10, 20, 20, 20, 30, 40};
+    int key = 20;
+
+    int start = 0, end = 5;
+    int ans = -1;
+
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] < key){
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    if (ans != -1) {
+        cout << "Element Found at index = " << ans;
+    }
+    else {
+        cout << "Element Not Found";
+    }
+    
+    return 0;
+    
+}
