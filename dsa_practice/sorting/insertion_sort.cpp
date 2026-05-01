72004673341ff6faecb6d6db42f72bc9bcc65571
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-05-01 20:00:46 +0530

    Day 9 completed shorting (selection, bubble, insertion)

--- dsa_practice/sorting/insertion_sort.cpp
+++ dsa_practice/sorting/insertion_sort.cpp
@@ -1,44 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n)
-{
-    for (int i = 1; i < n; i++)
-    {
-        int key = arr[i];
-        int j = i - 1;
+int main(){
+    int arr [5] = {5, 3, 2, 4, 1};
 
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
+    for (int i=1; i<5; i++){
+        int key = arr[i];
+        int j = i-1;
+
+        while (j>=0 && arr[j] > key){
+            arr[j+1] = arr[j];
             j--;
         }
-        arr[j + 1] = key;
+        arr[j+1] = key;
     }
-}
-
-int main()
-{
-    cout << "=== Insertion Sort ===\n";
-
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 7;
-
-    cout << "Original Array: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+    for (int i=0; i<5; i++){
+        cout << arr[i] << endl;
     }
-    cout << "\n\n";
-
-    insertionSort(arr, n);
-
-    cout << "Sorted Array:   ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << "\n";
-
     return 0;
 }
