14d0b8b35ba7cba1c87176747372db0104c331db
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-05-01 19:46:26 +0530

    update

--- dsa_practice/sorting/selection_sort.cpp
+++ dsa_practice/sorting/selection_sort.cpp
@@ -1,49 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int minIdx = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[minIdx])
-            {
-                minIdx = j;
+int main(){
+    int arr[5] = {64, 25, 12, 22, 11};
+
+    for(int i = 0; i <5; i++){
+        int minIndex = i;
+
+        for(int j = i+1; j < 5; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
             }
         }
-        if (minIdx != i)
-        {
-            int temp = arr[i];
-            arr[i] = arr[minIdx];
-            arr[minIdx] = temp;
-        }
+
+        swap(arr[i], arr[minIndex]);
     }
-}
-
-int main()
-{
-    cout << "=== Selection Sort ===\n";
-
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 7;
-
-    cout << "Original Array: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << endl;
     }
-    cout << "\n\n";
-
-    selectionSort(arr, n);
-
-    cout << "Sorted Array:   ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << "\n";
-
     return 0;
 }
