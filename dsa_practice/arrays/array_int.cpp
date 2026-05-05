9a2ab4d499f098649d89bb549b6050b7c1f7ebb3
sahuvaibhav07 <sahuvaibhav06@gmail.com>
2026-05-05 19:47:09 +0530

    Array practice

---
+++ dsa_practice/arrays/array_int.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << "Array elements are: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }   cout << endl;
+    return 0;
+}
